4be2eb7c49288df76a335e2bd6a403e82cb9689f
lujingwei <lujingwei002@qq.com>
2023-11-15 11:07:31 +0800

    修改gen路径

---
+++ bin/gen-server/cmd/root.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/lujingwei002/gen_server"
+	"github.com/lujingwei002/gen_server/bin/gen-server/cmd/gen"
+	"github.com/lujingwei002/gen_server/bin/gen-server/cmd/gen_behavior"
+	"github.com/lujingwei002/gen_server/bin/gen-server/cmd/gen_const"
+	"github.com/lujingwei002/gen_server/bin/gen-server/cmd/gen_model"
+	"github.com/lujingwei002/gen_server/bin/gen-server/cmd/gen_resource"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	configFile string
+	rootCmd    = &cobra.Command{
+		Use:   "gen_server",
+		Short: "Short",
+		Long: `L
+O
+N
+G`,
+		Run: func(cmd *cobra.Command, args []string) {
+			// Do Stuff Here
+		},
+	}
+)
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.AddCommand(gen.Cmd)
+	rootCmd.AddCommand(gen_resource.Cmd)
+	rootCmd.AddCommand(gen_const.Cmd)
+	rootCmd.AddCommand(gen_model.Cmd)
+	rootCmd.AddCommand(gen_behavior.Cmd)
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func initConfig() {
+	if configFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(configFile)
+	} else {
+		// Search config in home directory with name ".gen_server" (without extension).
+		viper.AddConfigPath(".")
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".gen_server.yaml")
+	}
+	viper.SetEnvPrefix("gen_server")
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+	var c gen_server.Config
+	err := viper.Unmarshal(&c)
+	if err != nil {
+		log.Fatalf("unable to decode into struct, %v", err)
+	}
+}
--- makefile
+++ makefile
@@ -1,14 +1,14 @@
 
 
-all: gen_server gen_grpc_server gen_grpc_client
+all: gen-server gen-grpc-server gen-grpc-client
 
-gen_server:
-	go build -o gen_server bin/gen_server/main.go
+gen-server:
+	go build -o gen-server bin/gen-server/main.go
 
 
-gen_grpc_server:
-	go build -o gen_grpc_server bin/gen_grpc_server/main.go
+gen-grpc-server:
+	go build -o gen-grpc-server bin/gen-grpc-server/main.go
 
-gen_grpc_client:
-	go build -o gen_grpc_client bin/gen_grpc_client/main.go
+gen-grpc-client:
+	go build -o gen-grpc-client bin/gen-grpc-client/main.go
 
